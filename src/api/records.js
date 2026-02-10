e6956937a141f17057da6e80c28bf60219ddf3d4
Froggigi <asahigigi@gmail.com>
2026-02-10 23:22:25 +0800

    記帳 API 改用統一 api 模組

--- src/api/records.js
+++ src/api/records.js
@@ -1,21 +1,21 @@
-import axios from './axios'
+import api from '../utils/api'
 
 // 取得所有記帳
 export const getRecords = () => {
-  return axios.get('/api/records')
+  return api.get('/api/records')
 }
 
 // 新增記帳
 export const createRecord = (data) => {
-  return axios.post('/api/records', data)
+  return api.post('/api/records', data)
 }
 
 // 更新記帳
 export const updateRecord = (id, data) => {
-  return axios.put(`/api/records/${id}`, data)
+  return api.put(`/api/records/${id}`, data)
 }
 
 // 刪除記帳
 export const deleteRecord = (id) => {
-  return axios.delete(`/api/records/${id}`)
+  return api.delete(`/api/records/${id}`)
 }
